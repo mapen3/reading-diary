9471d04819af1a9a9aec3cb7e2247c81e2191579
Abu Pengwah <60161806+mapen3@users.noreply.github.com>
2020-08-04 18:48:57 +1000

    Revision 1
    Added files and log files to check how many files were read

--- list_unreadfiles.m
+++ list_unreadfiles.m
@@ -3,3 +3,31 @@ text = string(fileread('diary.tex'));
 
 %extract keyname of the read documents
 newStr = extractBetween(text,"\record{","}");
+folderinfo = dir('library');
+[r,~] = size(folderinfo)
+vec = 1;
+pdfname = string();
+for i = 1:r
+    if (folderinfo(i).bytes>0 && folderinfo(i).isdir == 0)
+        pdfname(vec,:) = string(folderinfo(i).name);
+        vec = vec + 1;
+    end
+end
+%delete ".pdf" extension
+pdfname = erase(pdfname,".pdf");
+[r,~] = size(pdfname);
+vec = 1;
+unread = string();
+for i = 1:r
+    tf = contains(pdfname(i,:),newStr);
+    if (tf)
+        
+    else
+        unread(vec,:) = pdfname(i,:);
+        vec = vec + 1;
+    end
+end
+
+fid = fopen('filelog_unreadfiles.txt','w');
+fprintf(fid, '%s\n', unread);
+fclose(fid);
